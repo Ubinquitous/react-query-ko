c8e30b0a82902f8e4ba416fe295f077fbc5bac04
ubinquitous <ubinquitous1@gmail.com>
2024-08-06 10:52:31 +0900

    chore : setting theme.config.tsx in tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./theme.config.tsx",
   ],
   theme: {
     extend: {
