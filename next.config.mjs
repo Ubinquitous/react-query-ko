3cfb70d70af5a55e749e79236d87f2022e25fb75
ubinquitous <ubinquitous1@gmail.com>
2024-08-06 10:52:22 +0900

    chore : set next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+import { hostname } from "os";
+import path from "path";
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+  async redirects() {
+    return [
+      {
+        source: "/docs",
+        destination: "/docs/getting-started",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nextjs.org",
+      },
+    ],
+  },
+});
+
+const nextConfig = withNextra({});
 
 export default nextConfig;
